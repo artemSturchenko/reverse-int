f3c3e773c785477c12ac8dbb13d16ec96f716c49
Artem <53123402+artemSturchenko@users.noreply.github.com>
2023-01-08 17:16:16 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  const positiveNumber = n>0 ? n : n*(-1)
+  const txtNumber = positiveNumber.toString()
+  return parseInt(txtNumber.split('').reverse().join(''))
 }
+
